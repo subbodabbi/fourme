a60c08b03344578360f2f5ffcc0528cdafec605e
Amier Ariffin <muhdamierrosli@hotmail.com>
2017-01-12 17:59:35 +0800

    added user types, created a few html pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < Clearance::PasswordsController
+
+def create
+    @user = authenticate(params)
+
+    sign_in(@user) do |status|
+      if status.success?
+      	flash[:notice] = "Signed In!"
+        redirect_back_or url_after_create
+      else
+        respond_to do |format|
+          format.html { redirect_to sign_in_url }
+          format.js
+        end
+
+      end
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < Clearance::PasswordsController
+
+before_action :find_user, only: [:show, :edit, :update]
+respond_to :html, :js
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+ 
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Sucessfully registered to 4Me!"
+        redirect_back_or url_after_create
+      else
+        respond_with( @user, :layout => !request.xhr? )
+      end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Successfully updated"
+      redirect_to @user
+    else
+      flash[:danger] = "Sorry, something went wrong. Try again later."
+      render :edit
+    end
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+private
+
+ def user_params
+   params.require(:user).permit(:type, :email, :password, :first_name, :last_name, :information, :phone_number, :address, :gender, :birthday)
+ end
+
 end
