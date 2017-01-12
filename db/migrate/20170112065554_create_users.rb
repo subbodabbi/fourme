1b20530729324474e32f252bce8cad98c0e1b6be
Amier Ariffin <muhdamierrosli@hotmail.com>
2017-01-12 15:29:58 +0800

    migrated user and website table

--- db/migrate/20170112065554_create_users.rb
+++ db/migrate/20170112065554_create_users.rb
@@ -1,8 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :email, null: false
+      t.string :encrypted_password, limit: 128, null: false
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+      t.string :first_name, null:false
+      t.string :last_name, null:false
+      t.text   :information
+      t.string :address
+      t.string :phone_number
       t.timestamps null: false
     end
+
+    add_index :users, :email
+    add_index :users, :remember_token
   end
 end
